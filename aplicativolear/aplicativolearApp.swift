0888c7e1ad4e0f61e7fedc0820555895bf24973d
DylanSkywalker <gplutarco02@gmail.com>
2023-05-25 23:14:14 -0300

    Initial Commit

---
+++ aplicativolear/aplicativolearApp.swift
@@ -0,0 +1,17 @@
+//
+//  aplicativolearApp.swift
+//  aplicativolear
+//
+//  Created by Gabriel on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct aplicativolearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
