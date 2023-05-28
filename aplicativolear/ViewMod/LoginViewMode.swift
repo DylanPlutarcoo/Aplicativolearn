003df3d5faceb77f42bcf442c6dc165bb04dd031
DylanSkywalker <gplutarco02@gmail.com>
2023-05-28 16:35:12 -0300

    learn app

---
+++ aplicativolear/ViewMod/LoginViewMode.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewMode.swift
+//  aplicativolear
+//
+//  Created by Gabriel on 28/05/23.
+//
+
+import SwiftUI
+
+class LoginViewModel :ObservableObject{
+    func autenticarUser (_username: String, _password: String) {
+        
+        
+    }
+}
